9cbd97e88c0d4faf9f35ebabb10373bfa00c080a
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-14 14:08:42 -0400

    add class aliases

---
+++ init.php
@@ -0,0 +1,7 @@
+<?php
+
+class_alias(Winter\MailChimp\Plugin::class, RainLab\MailChimp\Plugin::class);
+
+class_alias(Winter\MailChimp\Components\Signup::class, RainLab\MailChimp\Components\Signup::class);
+
+class_alias(Winter\MailChimp\Models\Settings::class, RainLab\MailChimp\Models\Settings::class);
