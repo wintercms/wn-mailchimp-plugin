d266b4182cb03177b521dd4f3126433f622622f4
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-15 09:59:48 -0400

    prevent creating the class aliases when the original plugin is still installed

--- init.php
+++ init.php
@@ -1,7 +1,9 @@
 <?php
 
-class_alias(Winter\MailChimp\Plugin::class, RainLab\MailChimp\Plugin::class);
+if (!class_exists(RainLab\MailChimp\Plugin::class)) {
+    class_alias(Winter\MailChimp\Plugin::class, RainLab\MailChimp\Plugin::class);
 
-class_alias(Winter\MailChimp\Components\Signup::class, RainLab\MailChimp\Components\Signup::class);
+    class_alias(Winter\MailChimp\Components\Signup::class, RainLab\MailChimp\Components\Signup::class);
 
-class_alias(Winter\MailChimp\Models\Settings::class, RainLab\MailChimp\Models\Settings::class);
+    class_alias(Winter\MailChimp\Models\Settings::class, RainLab\MailChimp\Models\Settings::class);
+}
