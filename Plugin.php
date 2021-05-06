7af3d768b2c402f21062991cb4a4f0ea70ab1a10
Marc Jauvin <marc.jauvin@gmail.com>
2021-05-06 09:19:43 -0400

    add registerClassAliases() method

--- Plugin.php
+++ Plugin.php
@@ -51,4 +51,17 @@ class Plugin extends PluginBase
             ]
         ];
     }
+
+    public function registerClassAliases()
+    {
+        /**
+         * To allow compatibility with plugins that extend the original RainLab.MailChimp plugin,
+         * this will alias those classes to use the new Winter.MailChimp classes.
+         */
+        return [
+            \Winter\MailChimp\Plugin::class              => \RainLab\MailChimp\Plugin::class,
+            \Winter\MailChimp\Components\Signup::class   => \RainLab\MailChimp\Components\Signup::class,
+            \Winter\MailChimp\Models\Settings::class     => \RainLab\MailChimp\Models\Settings::class,
+        ];
+    }
 }
